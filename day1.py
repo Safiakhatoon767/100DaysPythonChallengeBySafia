41657eda4a2c7bac1a66622a551d25c0832df8a1
Safia Khatoon <114645192+Safiakhatoon767@users.noreply.github.com>
2024-12-11 14:41:58 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,38 @@
+Step 1: Install Python on Your System
+Begin by downloading and installing Python from the official Python website. Ensure you check the box to add Python to your system's PATH during installation.
+
+Step 2: Install Replit Using My Hashnode Blog
+Once Python is installed, follow the detailed steps provided in my Hashnode blog to set up Replit. Click the link below to get started:
+📖 Visit My Hashnode Blog
+Python Day 1: A Beginner's Guide with Practice Exercises by Safia Khatoon.
+
+What is a programming language?
+
+A programming language is a way to communicate with a computer. For example, we communicate with each other using languages like Hindi, English, Telugu, Urdu, Marathi, etc., so we can understand one another. Similarly, a computer understands programming languages.
+
+What is Python?
+
+Python is an open-source, platform-independent programming language.
+
+Features of python ?
+
+It is an open-source programming language.
+
+It is free to use.
+
+It is easy to understand.
+
+It is easy to use.
+
+It is platform-independent.
+
+What is python used for?
+
+Using Python, you can work on data science, machine learning, and more. You can also create web applications and do many other things.
+
+Why We Use Replit
+
+Replit is easy to use and makes sharing code with others simple.
+
+Note: REPL (Read-Evaluate-Print Loop)
+
